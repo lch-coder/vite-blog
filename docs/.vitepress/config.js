49f7acb680770e5b81ef933b4a626ba10ce25c93
lch <1660396419@qq.com>
2022-08-10 23:24:10 +0800

    feat:配置nav、sidebar、网站图标

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,49 @@
 export default {
-  title: "VitePress",
-  description: "Just playing around",
   base: "/vite-blog",
+  title: "VitePress",
+  description: "VitePress Blog",
+  head: [["link", { rel: "icon", href: "/lightning.svg" }]],
+  lastUpdated: true, //开启上次更新时间
+  appearance: true, //是否启用“暗模式”
+  //主题配置
+  themeConfig: {
+    siteTitle: "vite-blog",
+    logo: "/lightning.svg",
+    nav: [
+      {
+        text: "💻前端",
+        activeMatch: "/frontEnd/",
+        items: [
+          { text: "🟧 HTML", link: "../frontEnd/html/html" },
+          { text: "🟥 CSS", link: "../frontEnd/css/css" },
+          { text: "🟨 JavaScript", link: "../frontEnd/javaScript/javaScript" },
+          { text: "🟦 TypeScript", link: "../frontEnd/typeScript/typeScript" },
+          { text: "🟩 Vue", link: "../frontEnd/vue/vue" },
+        ],
+      },
+      {
+        text: "🧊部署",
+        link: "/deploy/index",
+      },
+    ],
+    //社交链接
+    socialLinks: [{ icon: "github", link: "https://github.com/lch-coder" }],
+    //侧边栏
+    sidebar: {
+      "/frontEnd/": getFrontEndSidebar(),
+    },
+  },
 };
+
+function getFrontEndSidebar() {
+  return [
+    {
+      text: "🟧 HTML",
+      collapsible: true,
+      items: [
+        { text: "HTML 学习路径", link: "/frontEnd/html/html" },
+        { text: "HTML 零碎笔记", link: "/FrontEnd/html/html" },
+      ],
+    },
+  ];
+}
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,29 @@
-# Hello VitePress
+---
+layout: home
+
+hero:
+  name: 个人博客
+  text: ''
+  tagline: 随手写点博客
+  image:
+    src: /lightning.svg
+    alt: 闪电
+  actions:
+    - theme: brand
+      text: ⚡️前端
+      link: /frontEnd/html/html
+    - theme: alt
+      text: View on Github
+      link: https://github.com/lch-coder
+
+features:
+  - icon: 📖
+    title: 随手记录工作遇到的问题
+    details: 持续更新中。。。
+  - icon: 💻
+    title: 💻 前端
+    details: 🟧HTML 🟥CSS 🟨Js 🟦Ts 🟩Vue
+  - icon: 💻
+    title: 🧊 部署
+
+---
